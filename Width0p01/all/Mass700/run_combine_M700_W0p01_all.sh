b93604ba6834817851bd05c88be09a9738479586
Yihui Lai <yihui.lai@cern.ch>
2021-11-04 22:27:28 -0400

    first commit

---
+++ Width0p01/all/Mass700/run_combine_M700_W0p01_all.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cd /data/users/yihuilai/combine/CMSSW_11_0_0/src/ 
+eval `scramv1 runtime -sh` 
+cd /data/users/yihuilai/test_code/WG_Analysis/Plotting/data/higgs/Width0p01/all/Mass700/ 
+combine -M AsymptoticLimits -m 700  -t -1 wgamma_test_mt_res_M700_W0p01_vvdijet.txt >& results_mt_res_M700_W0p01_all.txt
+cd - 
+echo "^.^ FINISHED ^.^" 
